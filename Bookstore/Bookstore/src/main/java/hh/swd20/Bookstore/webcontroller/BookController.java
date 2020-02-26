4e3cb1007054f7eb5e3d7fca7738913b16f74ba7
Michaela Karkia <michaela@karkia.me>
2020-02-26 13:57:06 +0200

    created booklist.html and added controller to show all books

--- Bookstore/Bookstore/src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
+++ Bookstore/Bookstore/src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
@@ -1,10 +1,22 @@
 package hh.swd20.Bookstore.webcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.swd20.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	@Autowired
+private BookRepository bookRepository;
+	
+	@GetMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";
+	}
 	
 	@GetMapping("/index")
 	public String allBooksView() {
@@ -12,3 +24,4 @@ public class BookController {
 	}
 
 }
+
